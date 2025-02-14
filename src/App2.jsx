c087fd839339375130b41e0b8857603278adb08d
Leonardo Sehn <leonardomsehn@gmail.com>
2025-02-13 22:42:57 -0300

    commit inicial e Header component

---
+++ src/App2.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const App2 = () => {
+  return <div>App2</div>
+  };
+
+  export default App2;
+
+  //rafce
+
+  // Arrow function
+  // const App2 = () => <h1>Ola mundo</h1>;
+
+  // components com PascalCase
+
+  // export default nao precisa mesmo nome
